e14ed2683c77b7a23b0e5934b3edec211ec286b0
jesusalzate <alejoa0415@hotmail.com>
2026-01-08 21:53:12 +0100

    Adding statistical Analysis

--- Dataloader.py
+++ Dataloader.py
@@ -21,12 +21,23 @@ class Dataloader:
         test_run: bool = False,
         input_shape: str = "(128, 128, 32)",
         rand_prob: int = 0.5,
-        partition_column: str = "partition",
+        partition_column: str = "split",
         config: dict = config_default,
     ):
         df = read_csv(path, sep=sep)
 
         n_classes = len(unique(df["csPC"].values))
+        df['image_t2'] = df['image_t2'].apply(lambda x: x.replace("ceib/", ""))
+        df['image_adc'] = df['image_adc'].apply(lambda x: x.replace("ceib/", ""))
+        df['image_dwi'] = df['image_dwi'].apply(lambda x: x.replace("ceib/", ""))
+
+        df['image_t2'] = df['image_t2'].apply(lambda x: x.replace("prueba/p0052021_reborn/", "p0052021/"))
+        df['image_adc'] = df['image_adc'].apply(lambda x: x.replace("prueba/p0052021_reborn/", "p0052021/"))
+        df['image_dwi'] = df['image_dwi'].apply(lambda x: x.replace("prueba/p0052021_reborn/", "p0052021/"))
+
+        df['image_t2'] = df['image_t2'].apply(lambda x: x.replace("prueba/", ""))
+        df['image_adc'] = df['image_adc'].apply(lambda x: x.replace("prueba/", ""))
+        df['image_dwi'] = df['image_dwi'].apply(lambda x: x.replace("prueba/", ""))
 
         self.groupby = df.groupby(partition_column)
 
@@ -51,9 +62,14 @@ class Dataloader:
                 spatial_size=eval(input_shape),
                 mode=("trilinear", "trilinear", "trilinear"),
             ),
-            transforms.ScaleIntensityd(keys=['t2','dwi_0','adc'], minv=0.0, maxv=1.0, allow_missing_keys=True),
+            transforms.ScaleIntensityRangePercentilesd(keys=['adc'], lower=5, upper=99.5,b_min=0.0, b_max=1.0,clip=True),
+            transforms.ScaleIntensityd(keys=['t2','dwi_0'], minv=0.0, maxv=1.0, allow_missing_keys=True),
             transforms.ConcatItemsd(keys=['t2','dwi_0','adc'], name="image", dim=0),
-            transforms.SelectItemsd(keys=["image", "label"])
+            transforms.SelectItemsd(keys=["image", "label"]),
+            transforms.RandFlipd(keys=["image"], spatial_axis=(0), prob=0.2),
+            transforms.RandRotate90d(keys=["image"], prob=0.2),
+            transforms.RandAffined(keys=["image"], rotate_range=[0.1, 0.1, 0.1], translate_range=[0.1, 0.1, 0.1], prob=0.2),
+            transforms.RandGaussianNoised(keys=["image"], std=0.1, mean=0.0, prob=0.2)
         ])
         self.val_transforms = transforms.Compose([
             transforms.LoadImaged(keys=img_columns, image_only=True,ensure_channel_first=True),
@@ -71,7 +87,8 @@ class Dataloader:
                 spatial_size=eval(input_shape),
                 mode=("trilinear", "trilinear", "trilinear"),
             ),
-            transforms.ScaleIntensityd(keys=['t2','dwi_0','adc'], minv=0.0, maxv=1.0, allow_missing_keys=True),
+            transforms.ScaleIntensityRangePercentilesd(keys=['adc'], lower=5, upper=99.5,b_min=0.0, b_max=1.0,clip=True),
+            transforms.ScaleIntensityd(keys=['t2','dwi_0'], minv=0.0, maxv=1.0, allow_missing_keys=True),
             transforms.ConcatItemsd(keys=['t2','dwi_0','adc'], name="image", dim=0),
             transforms.SelectItemsd(keys=["image", "label"])
         ])
--- Dataloader_Picai.py
+++ Dataloader_Picai.py
@@ -20,15 +20,21 @@ class Dataloader:
         img_columns=["t2", "adc", "dwi"],
         test_run: bool = False,
         input_shape: str = "(128, 128, 32)",
-        rand_prob: int = 0.5,
+        rand_prob: float = 0.5,
         partition_column: str = "partition",
         config: dict = config_default,
     ):
         df = read_csv(path, sep=sep)
 
+        df['filepath_t2w_cropped'] = df['filepath_t2w_cropped'].apply(lambda x: x.replace("/nvmescratch/ceib/Prostate", "/mnt/datalake/FISABIO_datalake"))
+        df['filepath_adc_cropped'] = df['filepath_adc_cropped'].apply(lambda x: x.replace("/nvmescratch/ceib/Prostate", "/mnt/datalake/FISABIO_datalake"))
+        df['filepath_hbv_cropped'] = df['filepath_hbv_cropped'].apply(lambda x: x.replace("/nvmescratch/ceib/Prostate", "/mnt/datalake/FISABIO_datalake"))
+        
         n_classes = len(unique(df["label"].values))
 
         self.groupby = df.groupby(partition_column)
+        print(f"Number of partitions: {len(self.groupby)}")
+        print(f"Partitions: {self.groupby.groups.keys()}")
 
         self._class_weights = compute_class_weight(
             class_weight="balanced",
@@ -51,9 +57,14 @@ class Dataloader:
                 spatial_size=eval(input_shape),
                 mode=("trilinear", "trilinear", "trilinear"),
             ),
-            transforms.ScaleIntensityd(keys=['t2','dwi_0','adc'], minv=0.0, maxv=1.0, allow_missing_keys=True),
+            transforms.ScaleIntensityRangePercentilesd(keys=['adc'], lower=5, upper=99.5,b_min=0.0, b_max=1.0,clip=True),
+            transforms.ScaleIntensityd(keys=['t2','dwi_0'], minv=0.0, maxv=1.0, allow_missing_keys=True),
             transforms.ConcatItemsd(keys=['t2','dwi_0','adc'], name="image", dim=0),
-            transforms.SelectItemsd(keys=["image", "label"])
+            transforms.SelectItemsd(keys=["image", "label"]),
+            transforms.RandFlipd(keys=["image"], spatial_axis=(0), prob=0.2),
+            transforms.RandRotate90d(keys=["image"], prob=0.2),
+            transforms.RandAffined(keys=["image"], rotate_range=[0.1, 0.1, 0.1], translate_range=[0.1, 0.1, 0.1], prob=0.2),
+            transforms.RandGaussianNoised(keys=["image"], std=0.1, mean=0.0, prob=0.2)
         ])
         self.val_transforms = transforms.Compose([
             transforms.LoadImaged(keys=img_columns, image_only=True,ensure_channel_first=True),
@@ -71,7 +82,8 @@ class Dataloader:
                 spatial_size=eval(input_shape),
                 mode=("trilinear", "trilinear", "trilinear"),
             ),
-            transforms.ScaleIntensityd(keys=['t2','dwi_0','adc'], minv=0.0, maxv=1.0, allow_missing_keys=True),
+            transforms.ScaleIntensityRangePercentilesd(keys=['adc'], lower=5, upper=99.5,b_min=0.0, b_max=1.0,clip=True),
+            transforms.ScaleIntensityd(keys=['t2','dwi_0'], minv=0.0, maxv=1.0, allow_missing_keys=True),
             transforms.ConcatItemsd(keys=['t2','dwi_0','adc'], name="image", dim=0),
             transforms.SelectItemsd(keys=["image", "label"])
         ])
